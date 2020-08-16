26524788409711d026b2848034c3e73c2e33afa7
nitin55220 <ankur55420@gmail.com>
2020-08-16 22:09:11 +0530

    Updates

---
+++ precache-manifest.fb958ce3fa51a5c53fc5dec210b7ed0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474ec1cdd5d85cc8b26ef543ddd741b9",
+    "url": "/robot-attack/index.html"
+  },
+  {
+    "revision": "3ef8d53592356fa7c716",
+    "url": "/robot-attack/static/css/main.f5105599.chunk.css"
+  },
+  {
+    "revision": "cdc9b57c16ea4026460e",
+    "url": "/robot-attack/static/js/2.c6ad7e90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot-attack/static/js/2.c6ad7e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef8d53592356fa7c716",
+    "url": "/robot-attack/static/js/main.e3b61b2d.chunk.js"
+  },
+  {
+    "revision": "42f997217f214aef8a10",
+    "url": "/robot-attack/static/js/runtime-main.22f34062.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robot-attack/static/media/logo.5d5d9eef.svg"
+  }
+]);
